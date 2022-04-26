197b04e39c022c619d88b2430ee265feeb51f420
João Victor <jhonm4stercel@gmail.com>
2022-04-26 00:09:37 -0300

    Adicionado o arquivo inicial de run.

---
+++ run.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+use Discord\Discord;
+use Discord\WebSockets\Intents;
+use Dotenv\Dotenv;
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$discord = new Discord([
+    'token' => $_ENV['TOKEN'],
+    'storeMessages' => true,
+    'pmChannels' => true,
+    'loadAllMembers' => true,
+    'intents' => Intents::getDefaultIntents() | Intents::GUILD_MEMBERS,
+]);
+
+$discord->on('ready', function(Discord $discord) {
+
+});
+
+$discord->run();
